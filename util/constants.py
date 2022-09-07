4f55829a1533ef259a8df75bdd56cf17644860c7
Inna <idinna91@mail.ru>
2022-09-07 15:26:13 +0300

    added new class Constants

---
+++ util/constants.py
@@ -0,0 +1,16 @@
+class Constants:
+    PATH_TO_FILE_ADD_PET = 'request_body_json/add_new_pet.json'
+    PATH_TO_FILE_PLACE_ORDER = 'request_body_json/place_an_order.json'
+    CODE_SUCCESS = 200
+    CODE_ERROR = 404
+    VALUE_KEY_AVAILABLE = 'available'
+    KEY_MESSAGE = 'message'
+    KEY_ID = 'id'
+    KEY_NAME = 'name'
+    VALUE_NAME = 'Crispy'
+    KEY_PET_ID = 'petId'
+    KEY_QUANTITY = 'quantity'
+    KEY_STATUS = 'status'
+    VALUE_KEY_STATUS_PLACED = "placed"
+    VALUE_KEY_PET_NOT_FOUND = 'Pet not found'
+    QUANTITY_RANDOM_STATUS = 1
